5ffbacc33cbe22e7feb6bdd950f6a48669580879
Daniel Gery <17103444@l1847128.portoalegre.pucrsnet.br>
2018-04-13 16:41:50 -0300

    Cliente Sócio Teste

--- Atividade2_GCS_PUCRS/src/tests/ClienteSocioTest.java
+++ Atividade2_GCS_PUCRS/src/tests/ClienteSocioTest.java
@@ -2,27 +2,27 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.Serializable;
-
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import business.Cliente;
+import business.ClienteSocio;
 
-class ClienteTest{
-
-	private Cliente cli;
+class ClienteSocioTest {
+	private ClienteSocio cli;
 	private String nome = "nome";
 	private String CPF = "1234567890";
 	private char genero = 'M';
 	private int idade = 42;
 	private boolean status = false;
+	private int numero = 568;
+	private double milhagens = 22;
 
 	@Before
-	public void set() {cli = new Cliente(nome, CPF, genero, idade);}
+	public void set() {cli = new ClienteSocio(nome, CPF, genero, idade, numero, milhagens);}
 
 	@Test
-	void testNome() {assertEquals(nome, cli.getNome());}
+	void testNome() {assertEquals(nome, cli.getNome());}	
 
 	@Test
 	void testCPF() {assertEquals(CPF, cli.getCpf());}
@@ -36,5 +36,7 @@ class ClienteTest{
 	@Test
 	void testStatus() {cli.setStatus(status); assertEquals(status, cli.getStatus());}
 	
+	@Test
+	void testMilhagem() {cli.adicionarMilhagens(milhagens); assertEquals(milhagens, cli.getMilhagens());}
 
 }
